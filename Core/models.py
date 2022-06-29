0bb2a67b575b4b9c0c581c0d84069f78f007b250
B4ch0 <50328781+B4ch0@users.noreply.github.com>
2022-06-29 23:53:29 +0200

    klijenti v2

--- Core/models.py
+++ Core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=16, null=True, blank=True, verbose_name="Ime")
+    first_name = models.CharField(max_length=16,  verbose_name="Ime")
     last_name = models.CharField(max_length=16, null=True, blank=True, verbose_name="Prezime")
     address = models.CharField(max_length=64, null=True, blank=True, verbose_name="Adresa")
     OIB = models.CharField(max_length=11, null=True, blank=True)
