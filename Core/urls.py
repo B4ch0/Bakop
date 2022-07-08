55b0ed88a5476290e710698fbdf0612dd47c011d
B4ch0 <50328781+B4ch0@users.noreply.github.com>
2022-07-08 14:39:49 +0200

    kli+usl view path

--- Core/urls.py
+++ Core/urls.py
@@ -8,3 +8,5 @@ urlpatterns = [
     path('usluge/', name='service-list'),
     path('usluga/nova/', name='service-new'),
     path('klijent/novi/' , name='client-new'),
+    path('klijent/<int:pk>/', name='client-detail'),
+    path('usluga/<int:pk>/', name='service-detail'),
