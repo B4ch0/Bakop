69c873e238b98fe6341239f470f444e9308bd544
B4ch0 <50328781+B4ch0@users.noreply.github.com>
2022-07-12 00:05:39 +0200

    kl+usl detail view

--- Core/views.py
+++ Core/views.py
@@ -2,9 +2,13 @@
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic.edit import CreateView , UpdateView
+from django.shortcuts import redirect, render
 
 from .models import Client, Service
 
+
+def home(request):
+    return redirect("invoice-list")  
 
 class ClientCreateView(CreateView):
 
@@ -21,6 +25,19 @@ class ServiceCreateView(CreateView):
     def get_success_url(self, **kwargs):
         return reverse('service-detail', args=[self.object.pk]) 
 
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ['first_name', 'last_name' , 'address','OIB']
+    def get_success_url(self, **kwargs):
+        return reverse('client-detail', args=[self.object.pk]) 
+
+
+class ServiceUpdateView(UpdateView):
+    model = Service
+    fields = ['service_name', 'price']
+    def get_success_url(self, **kwargs):
+        return reverse('service-detail', args=[self.object.pk]) 
 
 class ClientUpdateView(UpdateView):
     model = Client
