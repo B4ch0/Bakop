f1861f43e4aa9f2ef13aa241dfbe44eef7c316e0
B4ch0 <50328781+B4ch0@users.noreply.github.com>
2022-07-13 16:03:26 +0200

    kl+usl view v3

--- Core/urls.py
+++ Core/urls.py
@@ -4,9 +4,29 @@ from django.urls import path
 urlpatterns = [
     path("", home, name="home"),
     
-    path('klijenti/', name='client-list'),
-    path('usluge/', name='service-list'),
-    path('usluga/nova/', name='service-new'),
-    path('klijent/novi/' , name='client-new'),
-    path('klijent/<int:pk>/', name='client-detail'),
-    path('usluga/<int:pk>/', name='service-detail'),
+    path('klijenti/', login_required(ClientListView.as_view(
+        template_name='Core/client-list.html')), name='client-list'),
+    path('usluge/', login_required(ServiceListView.as_view(
+        template_name='Core/service-list.html')), name='service-list'),
+
+
+   
+
+    path('klijent/<int:pk>/', login_required(ClientDetailView.as_view(
+        template_name='Core/client-detail.html')), name='client-detail'),
+    path('usluga/<int:pk>/', login_required(ServiceDetailView.as_view(
+        template_name='Core/service-detail.html')), name='service-detail'),
+   
+
+     path('klijent/izmjena/<int:pk>/', login_required(ClientUpdateView.as_view(
+        template_name='Core/client-update.html')), name='client-update'),
+    path('usluga/izmjena/<int:pk>/', login_required(ServiceUpdateView.as_view(
+        template_name='Core/service-update.html')), name='service-update'),
+   
+   
+   
+    path('klijent/novi/', login_required(ClientCreateView.as_view(
+        template_name='Core/client-new.html')), name='client-new'),
+    path('usluga/nova/', login_required(ServiceCreateView.as_view(
+        template_name='Core/service-new.html')), name='service-new'),
+    
--- Invoice/settings.py
+++ Invoice/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'Core',
     'tailwind',
+    'extra_views',
 ]
 
 MIDDLEWARE = [
